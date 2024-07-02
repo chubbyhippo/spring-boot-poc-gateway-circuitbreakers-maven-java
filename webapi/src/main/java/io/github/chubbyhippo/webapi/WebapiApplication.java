7b3b3e858e96518f500073a7c69ad3c0821a6d70
ch <mkmsacc@outlook.com>
2024-07-02 20:47:28 +0700

    Add error endpoint to HelloController
    An error endpoint ("/error") has been added to the HelloController class. This endpoint intentionally throws an IllegalStateException with the message "error", presumably for testing error handling mechanisms in the application.

--- webapi/src/main/java/io/github/chubbyhippo/webapi/WebapiApplication.java
+++ webapi/src/main/java/io/github/chubbyhippo/webapi/WebapiApplication.java
@@ -6,27 +6,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class WebapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebapiApplication.class, args);
+    }
 
 }
 
 @RestController
 class HelloController {
-	@GetMapping("/hello")
-	String getHello(){
-		return "Hello World";
-	}
+    @GetMapping("/hello")
+    String getHello() {
+        return "Hello World";
+    }
 
+    @GetMapping("/delayedhello")
+    String getDelayedHello() throws InterruptedException {
+        Thread.sleep(Duration.ofSeconds(5));
+        return "Hello World";
+    }
 
-	@GetMapping("/delayedhello")
-	String getDelayedHello() throws InterruptedException {
-		Thread.sleep(Duration.ofSeconds(5));
-		return "Hello World";
-	}
+    @GetMapping("/error")
+    String getErroredHello() {
+        throw new IllegalStateException("error");
+    }
 }

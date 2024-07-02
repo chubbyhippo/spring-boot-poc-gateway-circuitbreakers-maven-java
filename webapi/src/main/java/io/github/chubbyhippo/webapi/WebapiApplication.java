d04dc9b29d0c0c9f7bed6f5fa3e74c51c5225054
ch <mkmsacc@outlook.com>
2024-07-02 20:47:42 +0700

    Add error endpoint to HelloController
    An error endpoint ("/error") has been added to the HelloController class. This endpoint intentionally throws an IllegalStateException with the message "error", presumably for testing error handling mechanisms in the application.

--- webapi/src/main/java/io/github/chubbyhippo/webapi/WebapiApplication.java
+++ webapi/src/main/java/io/github/chubbyhippo/webapi/WebapiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
-import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class WebapiApplication {

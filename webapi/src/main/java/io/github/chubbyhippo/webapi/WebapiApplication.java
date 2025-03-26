b7900ab76d290f7bf9610fa19440a4db4cbe1579
chubbyhippo <mkmsacc@outlook.com>
2025-03-26 17:49:47 +0700

    Remove HelloController from WebapiApplication
    The HelloController and its endpoints have been deleted to streamline the WebapiApplication. This reduces unnecessary code and potentially unused endpoints, improving maintainability and simplifying the application structure.

--- webapi/src/main/java/io/github/chubbyhippo/webapi/WebapiApplication.java
+++ webapi/src/main/java/io/github/chubbyhippo/webapi/WebapiApplication.java
@@ -2,10 +2,6 @@ package io.github.chubbyhippo.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.Duration;
 
 @SpringBootApplication
 public class WebapiApplication {
@@ -16,26 +12,3 @@ public class WebapiApplication {
 
 }
 
-@RestController
-class HelloController {
-    @GetMapping("/hello")
-    String getHello() {
-        return "Hello!";
-    }
-
-    @GetMapping("/delayedhello")
-    String getDelayedHello() throws InterruptedException {
-        Thread.sleep(Duration.ofSeconds(5));
-        return "Hello! from delayed";
-    }
-
-    @GetMapping("/error")
-    String getErroredHello() {
-        throw new IllegalStateException("error");
-    }
-
-    @GetMapping("/fallback")
-    String getFallbackHello() {
-        return "Hello! from fallback";
-    }
-}
